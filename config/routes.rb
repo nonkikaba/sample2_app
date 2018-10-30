eb4015e10c9eeeef22219eba4f703167c335004c
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-30 23:46:44 +0900

    5shou

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   #=> root_path -> '/'
   get  '/help',    to: 'static_pages#help'
   #=> help_path -> '/help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
