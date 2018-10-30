eccbb29dd0fa3d0d4cd1dc2f2bddc3a15dc90965
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-30 23:35:15 +0900

    5shou

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
   root 'static_pages#home'
+  #=> root_path -> '/'
+  get  '/help',    to: 'static_pages#help'
+  #=> help_path -> '/help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
