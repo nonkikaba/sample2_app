99de796f0febec1a29576e29eabd850c6caa9f22
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-10-31 23:24:01 +0900

    6章

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, 
+                    format: { with: VALID_EMAIL_REGEX }
+                    # formatオプションは引数に正規表現を取る 
 end
