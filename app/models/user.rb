3ca5bf5c60219319b20106864e7d469a74c17afb
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-11-01 11:26:26 +0900

    6shou一意性

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  # データベース側で大文字と小文字の区別ができる保証がない。
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, 
-                    format: { with: VALID_EMAIL_REGEX }
-                    # formatオプションは引数に正規表現を取る 
+                    format: { with: VALID_EMAIL_REGEX },
+                    # formatオプションは引数に正規表現を取る
+                    uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -49,4 +49,14 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+
+  # 重複するメールアドレス拒否のテスト
+  # 大文字小文字を区別しない、一意性のテスト
+  # 通常メールアドレスは大文字小文字を区別しない
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
