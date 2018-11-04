c6bbec748d9dbe9ba0baa09c296923b56d1fc078
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-11-04 09:20:58 +0900

    7章

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    # params[:id]は文字列型の "1" ですが、findメソッドでは自動的に整数型に変換されます
+  end
+  
   def new
   end
 end
